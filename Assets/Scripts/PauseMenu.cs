6b02fcc002f5eaa778d4a3c792a8ee0bbfaa9096
Anna Filina <anyta-filina@yandex.ru>
2026-05-31 22:58:59 +0300

    PauseMenuPrototype + UI Prototype

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public MonoBehaviour playerController;
+
+    private bool isPaused = false;
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+
+        if (playerController != null)
+            playerController.enabled = false;
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+
+        if (playerController != null)
+            playerController.enabled = true;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
